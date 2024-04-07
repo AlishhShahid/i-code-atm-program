dca8eceed747ac0bc4608ccf9774ad31cfb55fc2
AlishhShahid <alishbashahidzaheer14@gmail.com>
2024-04-07 11:22:09 +0500

    push my atm program to github

---
+++ index.js
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let myBalance = 20000; // In dollar
+let myPin = 11444;
+console.log("AlishhShahid - ATM machine!");
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "Enter your pin code:",
+        type: "number",
+    },
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("Correct pin code!");
+    let operationAnswer = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "Select an operation:",
+            type: "list",
+            choices: ["withdraw", "check balance"],
+        },
+    ]);
+    if (operationAnswer.operation === "withdraw") {
+        let amountAnswer = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "Enter your amount:",
+                type: "number",
+            },
+        ]);
+        if (amountAnswer.amount > myBalance) {
+            console.log("Insufficient balance!!!");
+        }
+        else {
+            myBalance -= amountAnswer.amount;
+            console.log("Your remaining balance is: " + myBalance);
+        }
+    }
+    else if (operationAnswer.operation === "check balance") {
+        console.log("Your balance is: " + myBalance);
+    }
+}
+else {
+    console.log("Incorrect pin code!!!");
+}
